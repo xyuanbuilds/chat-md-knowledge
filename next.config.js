f63ea88f2eecc6501ee6134f2d21af643dfd89b7
xuyuan <xu.yuan@datatom.com>
2023-05-06 23:13:41 +0800

    chore: tauri basic

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  // Note: This feature is required to use NextJS Image in SSG mode.
+  // See https://nextjs.org/docs/messages/export-image-api for different workarounds.
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
